24f8a18dcfe060c1746e4bab9f92ba5c96bc16d7
Parth Tamane <Parth@Parths-MacBook-Pro.local>
2017-08-15 23:54:00 +0530

    CSV Parsing and all pokemon visible in view

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,36 @@
+ //
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Parth Tamane on 15/08/17.
+//  Copyright © 2017 Parth Tamane. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder:  aDecoder)
+    
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(_ pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+    
+    
+}
+ 
